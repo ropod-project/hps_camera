36f119556b45733fcd488c0dcaec5dab04c9eb6c
Patrick Nagel <patrick.nagel@smail.inf.h-brs.de>
2019-04-17 11:31:48 +0200

    Set camera parameters as rosparam.
    This commit allows to set all the available paramters of the API as rosparam in a launch file. The default values are taken from the factory settings (taken from the Windows GUI).

--- ros/src/ros_hps_camera.cpp
+++ ros/src/ros_hps_camera.cpp
@@ -1,7 +1,10 @@
+#include <string>
 #include "ros/ros.h"//ros
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/PointField.h>
 #include "../include/api.h"//api interface
+
+using namespace std;
 
 HPS3D_HandleTypeDef handle;
 
@@ -51,11 +54,13 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   camera_pub = n.advertise<sensor_msgs::PointCloud2>("hps_camera/depth/points", 1);
 
+  bool debugMode;
+  ros::param::param("~debugMode", debugMode, true);
+
   uint8_t fileName[10][20];
   uint32_t dev_cnt = 0;
   RET_StatusTypeDef ret = RET_OK;
 
-  // List the optional devices
   dev_cnt = HPS3D_GetDeviceList((uint8_t *)"/dev/", (uint8_t *)"ttyACM", fileName);
   handle.DeviceName = fileName[0];
 
@@ -87,8 +92,8 @@ int main(int argc, char **argv)
   sensor_msgs::PointCloud2 outputPointCloud;
   outputPointCloud.header.seq = seq;
   outputPointCloud.header.frame_id = "hps_camera";
-  outputPointCloud.height = RES_HEIGHT;//*measureData.point_cloud_data.height;
-  outputPointCloud.width = RES_WIDTH;//*measureData.point_cloud_data.width;
+  outputPointCloud.height = RES_HEIGHT;
+  outputPointCloud.width = RES_WIDTH;
   outputPointCloud.point_step = 12;
   outputPointCloud.row_step = RES_WIDTH * outputPointCloud.point_step;
   outputPointCloud.is_dense = true;
@@ -117,6 +122,155 @@ int main(int argc, char **argv)
 
   outputPointCloud.data.resize(RES_HEIGHT * RES_WIDTH * outputPointCloud.fields.size() * 4);
 
+  HDRConf hdrConf;
+  DistanceFilterConfTypeDef distanceFilterConf;
+  SmoothFilterConfTypeDef smoothFilterConf;
+  MountingAngleParamTypeDef mountingAngleParamConf;
+  // There is not setter for the opticalParamConf, yet.
+  OpticalParamConfTypeDef opticalParamConf;
+
+  int hdrConfHdrMode;
+  double hdrConfQualityOverexposed;
+  double hdrConfQualityOverexposedSerious;
+  double hdrConfQualityWeak;
+  double hdrConfQualityWeakSerious;
+  int hdrConfSimpleHdrMaxIntegration;
+  int hdrConfSimpleHdrMinIntegration;
+  int hdrConfSuperHdrFrameNumber;
+  int hdrConfSuperHdrMaxIntegration;
+  int hdrConfHdrDisableIntegrationTime;
+  ros::param::param("~hdrConfHdrMode", hdrConfHdrMode, 1);
+  ros::param::param("~hdrConfQualityOverexposed", hdrConfQualityOverexposed, 500.);
+  ros::param::param("~hdrConfQualityOverexposedSerious", hdrConfQualityOverexposedSerious, 800.);
+  ros::param::param("~hdrConfQualityWeak", hdrConfQualityWeak, 120.);
+  ros::param::param("~hdrConfQualityWeakSerious", hdrConfQualityWeakSerious, 80.);
+  ros::param::param("~hdrConfSimpleHdrMaxIntegration", hdrConfSimpleHdrMaxIntegration, 2000);
+  ros::param::param("~hdrConfSimpleHdrMinIntegration", hdrConfSimpleHdrMinIntegration, 200);
+  ros::param::param("~hdrConfSuperHdrFrameNumber", hdrConfSuperHdrFrameNumber, 3);
+  ros::param::param("~hdrConfSuperHdrMaxIntegration", hdrConfSuperHdrMaxIntegration, 20000);
+  ros::param::param("~hdrConfHdrDisableIntegrationTime", hdrConfHdrDisableIntegrationTime, 2000);
+  hdrConf.hdr_mode = static_cast<HDRModeTypeDef>(hdrConfHdrMode);
+  hdrConf.qualtity_overexposed = hdrConfQualityOverexposed;
+  hdrConf.qualtity_overexposed_serious = hdrConfQualityOverexposedSerious;
+  hdrConf.qualtity_weak = hdrConfQualityWeak;
+  hdrConf.qualtity_weak_serious = hdrConfQualityWeakSerious;
+  hdrConf.simple_hdr_max_integration = hdrConfSimpleHdrMaxIntegration;
+  hdrConf.simple_hdr_min_integration = hdrConfSimpleHdrMinIntegration;
+  hdrConf.super_hdr_frame_number = hdrConfSuperHdrFrameNumber;
+  hdrConf.super_hdr_max_integration = hdrConfSuperHdrMaxIntegration;
+  hdrConf.hdr_disable_integration_time = hdrConfHdrDisableIntegrationTime;
+  HPS3D_SetHDRConfig(&handle, hdrConf);
+
+  int distanceFilterConfFilterType;
+  double distanceFilterConfKalmanK;
+  int distanceFilterConfKalmanThreshold;
+  int distanceFilterConfNumCheck;
+  ros::param::param("~distanceFilterConfFilterType", distanceFilterConfFilterType, 0);
+  ros::param::param("~distanceFilterConfKalmanK", distanceFilterConfKalmanK, 0.3);
+  ros::param::param("~distanceFilterConfKalmanThreshold", distanceFilterConfKalmanThreshold, 200);
+  ros::param::param("~distanceFilterConfNumCheck", distanceFilterConfNumCheck, 2);
+  distanceFilterConf.filter_type = static_cast<DistanceFilterTypeDef>(distanceFilterConfFilterType);
+  distanceFilterConf.kalman_K = distanceFilterConfKalmanK;
+  distanceFilterConf.kalman_threshold = distanceFilterConfKalmanThreshold;
+  distanceFilterConf.num_check = distanceFilterConfNumCheck;
+  HPS3D_SetSimpleKalman(&handle, distanceFilterConf);
+
+  int smoothFilterConfType;
+  int smoothFilterConfArg1;
+  ros::param::param("~smoothFilterConfType", smoothFilterConfType, 0);
+  ros::param::param("~smoothFilterConfArg1", smoothFilterConfArg1, 0);
+  smoothFilterConf.type = static_cast<SmoothFilterTypeDef>(smoothFilterConfType);
+  smoothFilterConf.arg1 = smoothFilterConfArg1;
+  HPS3D_SetSmoothFilter(&handle, smoothFilterConf);
+
+  bool mountingAngleParamConfEnable;
+  int mountingAngleParamConfAngleVertical;
+  int mountingAngleParamConfHeight;
+  ros::param::param("~mountingAngleParamConfEnable", mountingAngleParamConfEnable, false);
+  ros::param::param("~mountingAngleParamConfAngleVertical", mountingAngleParamConfAngleVertical, 0);
+  ros::param::param("~mountingAngleParamConfHeight", mountingAngleParamConfHeight, 0);
+  mountingAngleParamConf.enable = mountingAngleParamConfEnable;
+  mountingAngleParamConf.angle_vertical = mountingAngleParamConfAngleVertical;
+  mountingAngleParamConf.height = mountingAngleParamConfHeight;
+  HPS3D_SetMountingAngleParamConf(&handle, mountingAngleParamConf);
+
+  int distanceOffset;
+  ros::param::param("~distanceOffset", distanceOffset, 0);
+  HPS3D_SetDistanceOffset(&handle, distanceOffset);
+
+  bool edgeDetectionEnable;
+  int edgeDetectionThresholdValue;
+  ros::param::param("~edgeDetectionEnable", edgeDetectionEnable, false);
+  ros::param::param("~edgeDetectionThresholdValue", edgeDetectionThresholdValue, 0);
+  HPS3D_SetEdgeDetectionEnable(edgeDetectionEnable);
+  HPS3D_SetEdgeDetectionValue(edgeDetectionThresholdValue);
+
+  if (debugMode)
+  {
+    HPS3D_GetHDRConfig(&handle, &hdrConf);
+    string currentHdrMode;
+    switch (hdrConf.hdr_mode)
+    {
+      case HDR_DISABLE:
+        currentHdrMode = "HDR_DISABLE";
+        break;
+      case AUTO_HDR:
+        currentHdrMode = "AUTO_HDR";
+        break;
+      case SUPER_HDR:
+        currentHdrMode = "SUPER_HDR";
+        break;
+      case SIMPLE_HDR:
+        currentHdrMode = "SIMPLE_HDR";
+        break;
+    }
+
+    printf("HDR configuration.\n HDR mode: %s %d\n Quality overexposed: %f\n Quality overexposed serious: %f\n Quality weak: %f\n Quality weak serious: %f\n Simple HDR max integration: %d\n Simple HDR min integration: %d\n Super HDR frame number: %d\n Super HDR max integration: %d\n HDR disable integration time: %d\n\n", currentHdrMode.c_str(), hdrConf.hdr_mode, hdrConf.qualtity_overexposed, hdrConf.qualtity_overexposed_serious, hdrConf.qualtity_weak, hdrConf.qualtity_weak_serious, hdrConf.simple_hdr_max_integration, hdrConf.simple_hdr_min_integration, hdrConf.super_hdr_frame_number, hdrConf.super_hdr_max_integration, hdrConf.hdr_disable_integration_time);
+
+    HPS3D_GetDistanceFilterConf(&handle, &distanceFilterConf);
+    string currentDistanceFilter;
+    switch (distanceFilterConf.filter_type)
+    {
+      case DISTANCE_FILTER_DISABLE:
+        currentDistanceFilter = "DISTANCE_FILTER_DISABLE";
+        break;
+      case DISTANCE_FILTER_SIMPLE_KALMAN:
+        currentDistanceFilter = "DISTANCE_FILTER_SIMPLE_KALMAN";
+        break;
+    }
+
+    printf("Distance filter configuration.\n Filter type: %s %d\n Kalman filter scale factor: %f\n Noise threshold: %d\n Threshold check frame number: %d\n\n", currentDistanceFilter.c_str(), distanceFilterConf.filter_type, distanceFilterConf.kalman_K, distanceFilterConf.kalman_threshold, distanceFilterConf.num_check);
+
+    HPS3D_GetSmoothFilterConf(&handle, &smoothFilterConf);
+    string currentSmoothFilter;
+    switch (smoothFilterConf.type)
+    {
+      case SMOOTH_FILTER_DISABLE:
+        currentSmoothFilter = "SMOOTH_FILTER_DISABLE";
+        break;
+      case SMOOTH_FILTER_AVERAGE:
+        currentSmoothFilter = "SMOOTH_FILTER_AVERAGE";
+        break;
+      case SMOOTH_FILTER_GAUSS:
+        currentSmoothFilter = "SMOOTH_FILTER_GAUSS";
+        break;
+    }
+
+    printf("Smooth filter configuration.\n Smoothing filter type: %s %d\n Filtering parameter %d\n\n", currentSmoothFilter.c_str(), smoothFilterConf.type, smoothFilterConf.arg1);
+
+    HPS3D_GetOpticalParamConf(&handle, &opticalParamConf);
+    printf("Optical param configuration.\n Enabled: %d\n Horizontal viewing angle: %d\n Vertical viewing angle: %d\n Horizontal illumination angle: %d\n Vertical illumination angle: %d\n\n", opticalParamConf.enable, opticalParamConf.viewing_angle_horiz, opticalParamConf.viewing_angle_vertical, opticalParamConf.illum_angle_horiz, opticalParamConf.illum_angle_vertical);
+
+    HPS3D_GetMountingParamConf(&handle, &mountingAngleParamConf);
+    printf("Mounting angle param configuration.\n Enabled: %d\n Vertical installation angle: %d\n Mounting height: %d\n\n", mountingAngleParamConf.enable, mountingAngleParamConf.angle_vertical, mountingAngleParamConf.height);
+
+    printf("Edge detection value: %d\n", HPS3D_GetEdgeDetectionValue());
+
+    int16_t offset;
+    HPS3D_GetDistanceOffset(&handle, &offset);
+    printf("Distance compensation: %d\n", offset);
+  }
+
   while(ros::ok())
   {
     if (handle.RunMode == RUN_SINGLE_SHOT)
@@ -126,6 +280,15 @@ int main(int argc, char **argv)
 			{
         if (handle.RetPacketType == FULL_DEPTH_PACKET)
         {
+
+          HDRConf hdrConf;
+          HPS3D_GetHDRConfig(&handle, &hdrConf);
+
+          if (debugMode && hdrConf.hdr_mode == AUTO_HDR)
+          {
+            printf("HDR disable integration time: %d\n\n", hdrConf.hdr_disable_integration_time);
+          }
+
           seq += 1;
           outputPointCloud.header.seq = seq;
           convertPointCloud(&handle, &outputPointCloud);
@@ -138,5 +301,6 @@ int main(int argc, char **argv)
     }
   }
 
+  HPS3D_RemoveDevice(&handle);
   return 0;
 }
